17d6b30de84070b7e535eff58c378544d85af2aa
Wy155 <yawwy-wp22@student.tarc.edu.my>
2025-03-06 10:05:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,37 +32,37 @@ features = [
 
 # Separate important and optional features
 important_features = [
-    "loan_percent_income",
-    "loan_int_rate",
-    "person_income",
-    "loan_amnt",
-    "person_age",
-    "cb_person_cred_hist_length"
+    ("loan_percent_income", float),
+    ("loan_int_rate", float),
+    ("person_income", float),
+    ("loan_amnt", float),
+    ("person_age", int),
+    ("cb_person_cred_hist_length", int)
 ]
 
 optional_features = [
-    "person_home_ownership_RENT",
-    "person_emp_length",
-    "cb_person_default_on_file_Y",
-    "person_home_ownership_OWN",
-    "loan_intent_HOMEIMPROVEMENT",
-    "loan_intent_MEDICAL",
-    "loan_intent_EDUCATION",
-    "loan_intent_PERSONAL",
-    "loan_intent_VENTURE",
-    "person_home_ownership_OTHER"
+    ("person_home_ownership_RENT", int),
+    ("person_emp_length", int),
+    ("cb_person_default_on_file_Y", int),
+    ("person_home_ownership_OWN", int),
+    ("loan_intent_HOMEIMPROVEMENT", int),
+    ("loan_intent_MEDICAL", int),
+    ("loan_intent_EDUCATION", int),
+    ("loan_intent_PERSONAL", int),
+    ("loan_intent_VENTURE", int),
+    ("person_home_ownership_OTHER", int)
 ]
 
 feature_inputs = []
 
 st.subheader("Key Information")
-for feature_name in important_features:
-    feature = st.number_input(feature_name, value=0.0)
+for feature_name, feature_type in important_features:
+    feature = st.number_input(feature_name, value=0, format="%d") if feature_type == int else st.number_input(feature_name, value=0.0)
     feature_inputs.append(feature)
 
 with st.expander("Additional Information (Optional)"):
-    for feature_name in optional_features:
-        feature = st.number_input(feature_name, value=0.0)
+    for feature_name, feature_type in optional_features:
+        feature = st.number_input(feature_name, value=0, format="%d") if feature_type == int else st.number_input(feature_name, value=0.0)
         feature_inputs.append(feature)
 
 # Prediction
